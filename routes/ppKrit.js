507b690bd9effeefeaa26e29ecc470f90424b41b
Thawatchai <yua.mch@gmail.com>
2023-03-13 13:06:52 +0700

    Edit Total luckyfans and check ID

--- routes/ppKrit.js
+++ routes/ppKrit.js
@@ -12,60 +12,57 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  let check = AllData.findOne(
-    { player_name: req.body.player_name },
-    (err, result) => {
-      if (result === null) {
-        console.log(true);
+  let check = AllData.findOne({ id: req.id }, (err, result) => {
+    if (result === null) {
+      console.log(true);
 
-        PPKrit.find((err, players) => {
-          if (err) return next(err);
-          //res.json(players);
-          if (req.body.status === 1) {
-            if (players.length < 10) {
-              const data = {
-                name: req.body.player_name,
-                status: req.body.status,
-                check: true,
-                tel: req.body.tel,
-                retail: req.body.retail,
-                id: req.body.id,
-              };
-              PPKrit.create(req.body, (err, post) => {
-                if (err) return next(err);
-                console.log("status 1");
-                res.json(data);
-                // res.send(data);
-              });
-            } else {
-              const data = { player_name: req.body.player_name, status: 0 };
-              res.send(data);
-            }
-          } else {
+      PPKrit.find((err, players) => {
+        if (err) return next(err);
+        //res.json(players);
+        if (req.body.status === 1) {
+          if (players.length < 25) {
             const data = {
-              player_name: req.body.player_name,
+              name: req.body.player_name,
               status: req.body.status,
               check: true,
               tel: req.body.tel,
               retail: req.body.retail,
               id: req.body.id,
             };
+            PPKrit.create(req.body, (err, post) => {
+              if (err) return next(err);
+              console.log("status 1");
+              res.json(data);
+              // res.send(data);
+            });
+          } else {
+            const data = { player_name: req.body.player_name, status: 0 };
             res.send(data);
           }
-        });
-      } else {
-        const data = {
-          player_name: req.body.player_name,
-          status: req.body.status,
-          check: false,
-          tel: req.body.tel,
-          retail: req.body.retail,
-          id: req.body.id,
-        };
-        res.send(data);
-      }
+        } else {
+          const data = {
+            player_name: req.body.player_name,
+            status: req.body.status,
+            check: true,
+            tel: req.body.tel,
+            retail: req.body.retail,
+            id: req.body.id,
+          };
+          res.send(data);
+        }
+      });
+    } else {
+      const data = {
+        player_name: req.body.player_name,
+        status: req.body.status,
+        check: false,
+        tel: req.body.tel,
+        retail: req.body.retail,
+        id: req.body.id,
+      };
+      res.send(data);
     }
-  );
+  });
 });
 
 router.delete("/", (req, res, next) => {
--- routes/tmd.js
+++ routes/tmd.js
@@ -12,60 +12,57 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  let check = AllData.findOne(
-    { player_name: req.body.player_name },
-    (err, result) => {
-      if (result === null) {
-        console.log(true);
+  let check = AllData.findOne({ id: req.body.id }, (err, result) => {
+    if (result === null) {
+      console.log(true);
 
-        Tmd.find((err, players) => {
-          if (err) return next(err);
-          //res.json(players);
-          if (req.body.status === 1) {
-            if (players.length < 10) {
-              const data = {
-                name: req.body.player_name,
-                status: req.body.status,
-                check: true,
-                tel: req.body.tel,
-                retail: req.body.retail,
-                id: req.body.id,
-              };
-              Tmd.create(req.body, (err, post) => {
-                if (err) return next(err);
-                console.log("status 1");
-                res.json(data);
-                // res.send(data);
-              });
-            } else {
-              const data = { player_name: req.body.player_name, status: 0 };
-              res.send(data);
-            }
-          } else {
+      Tmd.find((err, players) => {
+        if (err) return next(err);
+        //res.json(players);
+        if (req.body.status === 1) {
+          if (players.length < 50) {
             const data = {
-              player_name: req.body.player_name,
+              name: req.body.player_name,
               status: req.body.status,
               check: true,
               tel: req.body.tel,
               retail: req.body.retail,
               id: req.body.id,
             };
+            Tmd.create(req.body, (err, post) => {
+              if (err) return next(err);
+              console.log("status 1");
+              res.json(data);
+              // res.send(data);
+            });
+          } else {
+            const data = { player_name: req.body.player_name, status: 0 };
             res.send(data);
           }
-        });
-      } else {
-        const data = {
-          player_name: req.body.player_name,
-          status: req.body.status,
-          check: false,
-          tel: req.body.tel,
-          retail: req.body.retail,
-          id: req.body.id,
-        };
-        res.send(data);
-      }
+        } else {
+          const data = {
+            player_name: req.body.player_name,
+            status: req.body.status,
+            check: true,
+            tel: req.body.tel,
+            retail: req.body.retail,
+            id: req.body.id,
+          };
+          res.send(data);
+        }
+      });
+    } else {
+      const data = {
+        player_name: req.body.player_name,
+        status: req.body.status,
+        check: false,
+        tel: req.body.tel,
+        retail: req.body.retail,
+        id: req.body.id,
+      };
+      res.send(data);
     }
-  );
+  });
 });
 
 router.delete("/", (req, res, next) => {
